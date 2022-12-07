5f3671ab6b6c13fc064fd97ec603c3681cebaedc
LaLacou <108777796+llacourciere@users.noreply.github.com>
2022-12-06 22:55:11 -0800

    added cart function, actions and reducers and tests

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
+import { useStoreContext } from '../../utils/GlobalState';
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 function ProductItem(item) {
+
   const {
     image,
     name,
@@ -10,7 +13,15 @@ function ProductItem(item) {
     price,
     quantity
   } = item;
+  
+  const [state, dispatch] = useStoreContext();
 
+const addToCart = () => {
+  dispatch({
+    type: ADD_TO_CART,
+    product: { ...item, purchaseQuantity: 1 }
+  });
+};
   return (
     <div className="card px-1 py-1">
       <Link to={`/products/${_id}`}>
@@ -24,7 +35,7 @@ function ProductItem(item) {
         <div>{quantity} {pluralize("item", quantity)} in stock</div>
         <span>${price}</span>
       </div>
-      <button>Add to cart</button>
+      <button onClick={addToCart}>Add to cart</button>
     </div>
   );
 }
